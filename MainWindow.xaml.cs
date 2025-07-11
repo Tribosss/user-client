88b99c75c194cd9790e028cf631fa8d97a6d1f53
PKyes44 <rugate23@gmail.com>
2025-07-11 17:50:03 +0900

    feat: clear monitor code and nuget

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -23,23 +23,11 @@ namespace user_client
             // 트레이 초기화
             InitTray();
 
-            // 디바이스 초기화 및 시작
-            InitializeDevice();
-
             SignUpControl control = new SignUpControl();
             control.GotoSignInEvt += HandleGotoSignInControl;
             RootGrid.Children.Add(new SignUpControl());
         }
 
-        private void InitializeDevice()
-        {
-            LibPcapLiveDevice device = LibPcapLiveDeviceList.Instance[6];
-            
-            Console.WriteLine(device.ToString());
-            device.Open();
-            device.OnPacketArrival += Device_OnPacketArrival;
-            device.StartCapture();
-        }
         private void HandleGotoSignInControl()
         {
             RootGrid.Children.Clear();
@@ -119,131 +107,6 @@ namespace user_client
                 this.ShowInTaskbar = true;
                 tray.Visible = false;
             };
-        }
-
-        private void Device_OnPacketArrival(object s, PacketCapture e)
-        {
-            try { 
-                byte[]? payload = null;
-                string sourceIp, destIp;
-                int sourcePort = 0;
-                int destPort = 0;
-                string text;
-
-                byte[] rawBytes = e.GetPacket().Data;
-                LinkLayers linkLayerType = e.GetPacket().LinkLayerType;
-
-                // 패킷 파싱
-                Packet packet = Packet.ParsePacket(linkLayerType, rawBytes);
-
-                EthernetPacket? ether = packet.Extract<EthernetPacket>();
-                if (ether == null) return;
-
-                IPPacket? ip = packet.Extract<IPPacket>();
-                if (ip == null) return;
-                sourceIp = ip.SourceAddress.ToString();
-                destIp = ip.DestinationAddress.ToString();
-
-                // TCP/UDP 분류
-                switch (ip.Protocol)
-                {
-                    case ProtocolType.Udp:
-                        {
-                            UdpPacket udp = packet.Extract<UdpPacket>();
-                            if (udp == null) return;
-                            sourcePort = udp.SourcePort;
-                            destPort = udp.DestinationPort;
-                            payload = udp.PayloadData;
-                            break;
-                        }
-                    case ProtocolType.Tcp:
-                        {
-                            TcpPacket tcp = packet.Extract<TcpPacket>();
-                            sourcePort = tcp.SourcePort;
-                            destPort = tcp.DestinationPort;
-                            payload = tcp.PayloadData;
-                            break;
-                        }
-                }
-
-                if (sourcePort == 3306 || destPort == 3306) return;
-
-                // payload 추출
-                if (payload == null || payload.Length <= 0) return;
-                text = Encoding.ASCII.GetString(payload);
-
-                // 키워드 탐지
-                foreach (string keyword in _keywords)
-                {
-                    if (text.IndexOf(keyword, StringComparison.OrdinalIgnoreCase) < 0) continue;
-
-                    Console.WriteLine("======================");
-                    Console.WriteLine($"Detected Keyword: {keyword}");
-                    Console.WriteLine($"payload: {text}");
-
-                    string now = DateTime.Now.ToString("yyyy-MM-dd");
-
-                    SuspicionLog detectedLog = new SuspicionLog
-                    {
-                        Msg = $"Detected Keyword {keyword}",
-                        keyword = keyword,
-                        EmpId = 1234,
-                        SourceIp = sourceIp,
-                        DestIp = destIp,
-                        SourcePort = sourcePort,
-                        DestPort = destPort,
-                        DetectedAt = now,
-                    };
-
-
-                    insertSuspicionLog(detectedLog);
-
-                    break;
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Error processing packet: {ex.Message}");
-            }
-        }
-
-        private void insertSuspicionLog(SuspicionLog log)
-        {
-            try {
-                Env.Load();
-
-                string? host = Environment.GetEnvironmentVariable("DB_HOST");
-                if (host == null) return;
-                string? port = Environment.GetEnvironmentVariable("DB_PORT");
-                if (port == null) return;
-                string? uid = Environment.GetEnvironmentVariable("DB_UID");
-                if (uid == null) return;
-                string? pwd = Environment.GetEnvironmentVariable("DB_PWD");
-                if (pwd == null) return;
-                string? name = Environment.GetEnvironmentVariable("DB_NAME");
-                if (name == null) return;
-
-                string dbConnection = $"Server={host};Port={port};Database={name};Uid={uid};Pwd={pwd}";
-
-                using (MySqlConnection connection = new MySqlConnection(dbConnection))
-                {
-                    connection.Open();
-
-                    string query = "insert into suspicion_logs(msg, emp_id, source_ip, dest_ip, source_port, dest_port, keyword, detected_at) ";
-                    query += $"values ('{log.Msg}', 1, '{log.SourceIp}', '{log.DestIp}', {log.SourcePort}, {log.DestPort}, '{log.keyword}', '{log.DetectedAt}');";
-
-                    MySqlCommand cmd = new MySqlCommand(query, connection);
-
-                    if (cmd.ExecuteNonQuery() == 1) Console.WriteLine("Success Insert");
-                    else Console.WriteLine("Failed Insert");
-
-                    connection.Close();
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.ToString());
-            }
         }
     }
 }
